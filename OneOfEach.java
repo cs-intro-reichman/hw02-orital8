f8d39d8f9ba0a6e186594a43cef50c56840cfea1
orital8 <ori.tal01@post.runi.ac.il>
2023-12-27 19:02:38 +0200

    try 6

--- OneOfEach.java
+++ OneOfEach.java
@@ -5,6 +5,36 @@
  */
 public class OneOfEach {
 	public static void main (String[] args) {
-		//// Put your code here
+		int randomNumber1 = (int) (Math.random() * (10 )); 		//generate random number
+		boolean checkboy=false;									//0-4 for boy 5-9 for girl
+		boolean checkgirl=false;
+		if(randomNumber1 > 4)
+		checkgirl= true;			
+		else
+			checkboy=true;	
+		if (randomNumber1 <= 4){
+		System.out.print("b"); 
+		}
+		else{
+		System.out.print("g");
+		}
+		int i = 1;
+			while (checkboy == false || checkgirl == false){
+			randomNumber1 = (int) (Math.random() * (10 ));
+			if (randomNumber1<=4){
+			System.out.print("b");
+			checkboy= true;
+			i++;
+			}
+				else;
+				if (randomNumber1 > 4) {
+				System.out.print("g");
+				checkgirl=true;
+				i++;
+					}
+					else;
+				}
+				System.out.println();
+				System.out.print("You made it... and you now have "+ i +" children.");
 	}
 }
