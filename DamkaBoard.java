ff3f4587bee3d3bf23a6e74cc342b999323e51c4
orital8 <ori.tal01@post.runi.ac.il>
2023-12-27 17:53:46 +0200

    4 and 5

--- DamkaBoard.java
+++ DamkaBoard.java
@@ -3,6 +3,25 @@
  */
 public class DamkaBoard {
 	public static void main(String[] args) {
-		//// Put your code here
+		int num = Integer.parseInt(args[0]);   		//gets input 
+	
+	for(int i = 1; i <= num;i++){
+		if ( num % 2 != 0 ){
+			for(int j = 0; j < num;j++){
+			System.out.print("* ");
+				}
+				}
+			else
+			{
+			for(int m = 1; m <= num;m++){
+			System.out.print("* ");
+			}
+		}
+		System.out.println();
+		if (i%2 != 0)
+		System.out.print(" ");
+		else ;
+		}
+		}
 	}
-}
+
--- Perfect.java
+++ Perfect.java
@@ -3,6 +3,23 @@
  */
 public class Perfect {
 	public static void main (String[] args) {
-		//// Put your code here
-	}
+		int check = Integer.parseInt(args[0]);  
+		String str = check+ " is a perfect number since " +check+ " = 1 + ";		//string for saveing divisors
+		int counter = 1;
+		for (int i = 2; i < check;i++) {											//checking if i is divisor of num
+			if(check%i == 0){
+			counter = counter+i;													//counting the divisors
+			str = str+i+" + ";
+			}
+			else ;
+		}
+		if (counter == check){
+		String newString = str.substring(0, str.length() - 3);						//delete the last 3 characters from the string
+		System.out.println(newString);
+		}
+		else{
+		System.out.println(check+" is not a perfect number");						//noting if number is not perfect 
+		}
+		}
 }
+
