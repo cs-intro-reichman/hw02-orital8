e9e59b6f6ee35c7bcbb259daa285c4b085d0cd18
orital8 <ori.tal01@post.runi.ac.il>
2023-12-27 15:13:51 +0200

    two exrecise

--- Divisors.java
+++ Divisors.java
@@ -3,6 +3,13 @@
  */
 public class Divisors {
 	public static void main (String[] args) {
-		//// Put your code here
+		int num = Integer.parseInt(args[0]);   		//gets input 
+		
+	for (int i = 1; i<=num; i++) {				//checking if i is divisor of num
+		if (num%i==0)
+		System.out.println(i);
+		else ;
+						}
+		
 	}
 }
--- Reverse.java
+++ Reverse.java
@@ -4,6 +4,26 @@
  */
 public class Reverse {
 	public static void main (String[] args){
-		//// Put your code here
-	}
+	
+		String word = args[0];				
+		int length = word.length()-1;  					//checking word length, -1 for fit the index counter
+		int middle = (length/2);   						//pulls out the middle char location
+		if(length%2 != 0)									// check if length is odd
+			middle = middle+1;
+		else;
+			
+		char m = word.charAt(middle);					//pulls out the middle char 
+		int i = length;									//counter for while loop
+		String reverse="";								//variable for reverse string
+		
+			while (i>=0){								//loop for reverse the string
+		
+			char c = word.charAt(i);
+			reverse= reverse+c;
+			i=i-1;
+			
+			}
+			System.out.println(reverse);
+			System.out.println("The middle character is "+ m);
+		}
 }
